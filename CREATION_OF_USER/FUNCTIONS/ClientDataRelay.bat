78c980f412d45b20665adb2d49b1e29ee5eb7a88
JHONATAN STEVEN CARRILLO MEJIA <stevenmejia293@outlook.com>
2024-12-17 14:21:44 -0500

    ok

--- CREATION_OF_USER/FUNCTIONS/ClientDataRelay.bat
+++ CREATION_OF_USER/FUNCTIONS/ClientDataRelay.bat
@@ -2,6 +2,7 @@
 rem Script para ejecutar el acceso directo ClientDataRelayTest.lnk
 
 rem Ruta del acceso directo
+rem Esta ruta se debe cambiar a la del acceso directo a donde se encuentre el aplicativo.
 set shortcut_path="C:\Users\USER_1\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\ClientDataRelayTest.lnk"
 
 rem Ejecutar el acceso directo
